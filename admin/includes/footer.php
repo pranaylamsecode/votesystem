7f5acdc73b802d463a066d3e0273b89a88acd814
pranaylamsecode <pranaylamsecode@gmail.com>
2023-04-21 14:47:22 +0530

    first commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Online Voting System using Blockchain</b>
+    </div>
+    <strong> &copy; 2023 </strong>
+</footer>
---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>VOnline Voting System using Blockchain</b>
+      </div>
+      <strong></strong>
+    </div>
+    <!-- /.container -->
+</footer>
